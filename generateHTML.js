25894933d0ad79682a6efd67a3f3c856d8187387
JT Turner <j.tturner14@yahoo.com>
2020-03-15 13:52:25 -0400

    started generatehtml. pulled response data

---
+++ generateHTML.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+function generateHTML(data, color) {
+    const htmlTemplate = `
+     <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+body {
+    background-color: black;
+    }
+div {
+  color: red;
+  border: 2px solid;
+}
+p {
+    padding-left: 10px;
+    font-family: verdana;
+    color: white;
+}
+img  {
+  height: 200px;
+  width: 200px;
+  padding-top: 10px;
+  padding-left: 10px;
+}
+</style>
+</head>
+<body>
+<h1 style = 'color: ${color}'>${data.name}</h1>
+<div>    
+<img src = "${data.avatar_url}">
+    
+                <p>Username: ${data.login}</p>
+                <p>Followers: ${data.followers}</p>
+                <p>Following: ${data.following}</p>
+                <p>Repo URL: ${data.repos_url}</p>
+                <p>Bio: ${data.bio}</p>
+                <p>Location: ${data.location}</p>
+                <p>Avatar URL: ${data.avatar_url}</p>
+    </div>
+    
+</body>
+</html>
+     `
+     return htmlTemplate
+}
+
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const inquirer = require("inquirer");
-const generateHTML = require("./generateHTML")
+const generateHTML = require("./generateHTML");
 const fs = require("fs");
 
 inquirer.prompt([
@@ -20,4 +20,26 @@ inquirer.prompt([
             "purple"
         ]
     }
-])
+]).then(function(data) {
+    console.log(data.githubUsername);
+    console.log(data.colors);
+
+    axios.get("https://api.github.com/users/" + data.githubUsername)
+    .then(function(response) {
+        console.log(resonse.data);
+        console.log(resonse.data.login);
+        console.log(resonse.data.followers);
+        console.log(resonse.data.following);
+        console.log(resonse.data.repos_url);
+        console.log(resonse.data.bio);
+        console.log(resonse.data.location);
+        console.log(resonse.data.avatar_url);
+        const finishedHTML = generateHTML(response.data, data.colors);
+
+        fs.writeFile("./index.html", finishedHTML, function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    })
+});
