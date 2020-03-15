a73ec0a050c1b7b62d64b55ddaa234dd84a1fef5
JT Turner <j.tturner14@yahoo.com>
2020-03-15 13:36:35 -0400

    installed dependencies

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const inquirer = require("inquirer");
 const generateHTML = require("./generateHTML")
 const fs = require("fs");
 
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "githubUsername"
+    },
+    {
+        type: "list",
+        message: "What is your favorite color?",
+        name: "colors",
+        choices: [
+            "red",
+            "yellow",
+            "green",
+            "purple"
+        ]
+    }
+])
